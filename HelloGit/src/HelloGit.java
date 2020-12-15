e13cb9f824c9e2bf46a4f77fb2f714b850625429
HJ Gwak <pangu89@naver.com>
2020-12-15 23:40:18 +0900

    added major

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("HyeonJin Gwak");
+		System.out.println("Computer Software Engineering")
 	}
 }
